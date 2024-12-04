26851c4e9bf6cf7af43fca4cf55140c86f923d81
shamal20186010 <shamalwassana78@gmail.com>
2024-12-04 11:05:49 +0530

    add dependency and setup project

--- src/main/java/org/millennium_hardware/Main.java
+++ src/main/java/org/millennium_hardware/Main.java
@@ -1,7 +1,11 @@
 package org.millennium_hardware;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helllo");
+        SpringApplication.run(Main.class);
     }
 }
